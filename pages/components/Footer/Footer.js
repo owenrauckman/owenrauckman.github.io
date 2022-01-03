9be48a3350037b1c608a8ef9007f008d9ad76f01
Owen Rauckman <orauckman@fanthreesixty.com>
2022-01-02 18:46:45 -0600

    Adds footer module

---
+++ pages/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+function Header() {
+  return <div className={styles.footer}></div>;
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>Main</main>
-
-      <footer className={styles.footer}>foot</footer>
+      <main className={styles.main}></main>
     </div>
   );
 }
